c5fd7666c41d38fc731018933106c73bb3fa65c5
Esraa Madi <esraamadi@Esraas-MacBook-Pro.local>
2024-04-23 14:11:09 +0300

    add barcode

--- ulits/.ipynb_checkpoints/load_data-checkpoint.py
+++ ulits/.ipynb_checkpoints/load_data-checkpoint.py
@@ -57,7 +57,7 @@ def load_champian():
         headers = data[10] 
         data = [row[1:] for row in data[11:]] # delete first column 
         df = pd.DataFrame(data, columns=headers[1:])
-        df.dropna(inplace=True, axis=0)
+        df.dropna(how='all', inplace=True)
         # Store the DataFrame in a dictionary with the worksheet title as the key
         dataframes[worksheet.title] = df
 

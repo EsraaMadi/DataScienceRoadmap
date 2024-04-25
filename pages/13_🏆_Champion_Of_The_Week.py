6e430f76c56e00754063934f79a6814470c49a8c
Esraa Madi <esraamadi@Esraas-MacBook-Pro.local>
2024-04-25 13:16:28 +0300

    add barcode

--- pages/13_🏆_Champion_Of_The_Week.py
+++ pages/13_🏆_Champion_Of_The_Week.py
@@ -3,11 +3,11 @@ import ulits.load_data as ld
 st.set_page_config(layout='wide')
 
 # @st.cache_data(show_spinner="Fetching roadmap...")
-def _get_raw_chanpion():
-    df_lst = ld.load_champian()
+def _get_raw_chanpion(week_no):
+    df_lst = ld.load_champian(week_no)
     return df_lst
 
-Week_no = 3
+week_no = 3
 # show logo image
 _, im_col, _ = st.columns([0.35, 0.3, 0.35])
 with im_col:
@@ -17,7 +17,7 @@ st.markdown('#')
 st.markdown('#')
 
 # Get dictionery of each week
-df_dict = _get_raw_chanpion(Week_no)
+df_dict = _get_raw_chanpion(week_no)
 
 # aggregate all weeks data in one datafram
 df_students = ld.get_champions(df_dict)

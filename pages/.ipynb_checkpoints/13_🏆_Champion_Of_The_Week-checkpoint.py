759860169b6fc23ff0675b729280503cad0c2925
esraamadi <esraa.sabry.mohamed@gmail.com>
2024-05-26 12:28:58 +0300

    add barcode

--- pages/.ipynb_checkpoints/13_🏆_Champion_Of_The_Week-checkpoint.py
+++ pages/.ipynb_checkpoints/13_🏆_Champion_Of_The_Week-checkpoint.py
@@ -121,8 +121,8 @@ cols_row4 = st.columns(math.floor(row_col_no))
 
 
 for c, col in zip(cols_row3, df_students_i.columns):
-    df_sort = df_students_i[col].sort_values(col).head(5)
-    cols_row3[0].bar_chart(df_sort[['Name', col]])
+    df_sort = df_students_i[col].sort_values(col)
+    cols_row3[0].bar_chart(df_sort[['Name', col]].head(5))
 
 # display_matrices = _get_materic(df_students.columns[1:])
 # for c, col in zip(cols_row3, df_students_i.columns):

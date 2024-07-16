a1a1e742c2a6a66788a9bf4cf0a389e9da09b801
Esraa Madi <esraa.sabry.mohamed@gmail.com>
2024-07-16 21:33:10 +0300

    Update 🚀_Home.py

--- 🚀_Home.py
+++ 🚀_Home.py
@@ -14,7 +14,7 @@ st.markdown('#')
 st.markdown('#')
 
 app_env = os.getenv('G_TYPE')
-st.write("HI SOOOOOOSOOOO"+app_env)
+st.write("HI SOOOOOOS"+app_env)
 
 # show arabic welcome message
 st.markdown("""<h4 style="text-align: right">      

c70b5275f42bb8417e9cb0b7a8980927c555719b
esraamadi <esraa.sabry.mohamed@gmail.com>
2024-07-16 21:59:51 +0300

    fix keys

--- ulits/.ipynb_checkpoints/load_data-checkpoint.py
+++ ulits/.ipynb_checkpoints/load_data-checkpoint.py
@@ -5,33 +5,18 @@ import streamlit as st
 import os
 
 credentials_dict = {
-  "type": os.environ["type"],
-  "project_id": os.environ["project_id"],
-  "private_key_id": os.environ["private_key_id"],
-  "private_key": os.environ["private_key"],
-  "client_email": os.environ["client_email"],
-  "client_id": os.environ["client_id"],
-  "auth_uri": os.environ["auth_uri"],
-  "token_uri": os.environ["token_uri"],
-  "auth_provider_x509_cert_url": os.environ["auth_provider_x509_cert_url"],
-  "client_x509_cert_url": os.environ["client_x509_cert_url"],
-  "universe_domain": os.environ["universe_domain"]
+  "TYPE": os.getenv("TYPE"),
+  "PROJECT_ID": os.getenv("PROJECT_ID"),
+  "PRIVATE_KEY_ID": os.getenv("PRIVATE_KEY_ID"),
+  "PRIVATE_KEY": os.getenv("PRIVATE_KEY"),
+  "CLIENT_EMAIL": os.getenv("CLIENT_EMAIL"),
+  "CLIENT_ID": os.getenv("CLIENT_ID"),
+  "AUTH_URI": os.getenv("AUTH_URI"),
+  "TOKEN_URI": os.getenv("TOKEN_URI"),
+  "AUTH_PROVIDER_X509_CERT_URL": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+  "CLIENT_X509_CERT_URL": os.getenv("CLIENT_X509_CERT_URL"),
+  "UNIVERSE_DOMAIN": os.getenv("UNIVERSE_DOMAIN")
 }
-
-
-# credentials_dict = {
-#   "type": st.secrets["type"],
-#   "project_id": st.secrets["project_id"],
-#   "private_key_id": st.secrets["private_key_id"],
-#   "private_key": st.secrets["private_key"],
-#   "client_email": st.secrets["client_email"],
-#   "client_id": st.secrets["client_id"],
-#   "auth_uri": st.secrets["auth_uri"],
-#   "token_uri": st.secrets["token_uri"],
-#   "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
-#   "client_x509_cert_url": st.secrets["client_x509_cert_url"],
-#   "universe_domain": st.secrets["universe_domain"]
-# }
 
 # Define the scope
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']

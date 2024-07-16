3199fed281f882e7a4667c3a4b00f13ac8b551f9
esraamadi <esraa.sabry.mohamed@gmail.com>
2024-07-16 09:03:08 +0300

    fix keys

--- ulits/load_data.py
+++ ulits/load_data.py
@@ -2,20 +2,36 @@ import pandas as pd
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import streamlit as st
+import os
 
 credentials_dict = {
-  "type": st.secrets["type"],
-  "project_id": st.secrets["project_id"],
-  "private_key_id": st.secrets["private_key_id"],
-  "private_key": st.secrets["private_key"],
-  "client_email": st.secrets["client_email"],
-  "client_id": st.secrets["client_id"],
-  "auth_uri": st.secrets["auth_uri"],
-  "token_uri": st.secrets["token_uri"],
-  "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
-  "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+  "type": os.environ["type"],
+  "project_id": os.environ["project_id"],
+  "private_key_id": os.environ["private_key_id"],
+  "private_key": os.environ["private_key"],
+  "client_email": os.environ["client_email"],
+  "client_id": os.environ["client_id"],
+  "auth_uri": os.environ["auth_uri"],
+  "token_uri": os.environ["token_uri"],
+  "auth_provider_x509_cert_url": os.environ["auth_provider_x509_cert_url"],
+  "client_x509_cert_url": os.environ["client_x509_cert_url"],
   "universe_domain": st.secrets["universe_domain"]
 }
+
+
+# credentials_dict = {
+#   "type": st.secrets["type"],
+#   "project_id": st.secrets["project_id"],
+#   "private_key_id": st.secrets["private_key_id"],
+#   "private_key": st.secrets["private_key"],
+#   "client_email": st.secrets["client_email"],
+#   "client_id": st.secrets["client_id"],
+#   "auth_uri": st.secrets["auth_uri"],
+#   "token_uri": st.secrets["token_uri"],
+#   "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+#   "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+#   "universe_domain": st.secrets["universe_domain"]
+# }
 
 # Define the scope
 scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']

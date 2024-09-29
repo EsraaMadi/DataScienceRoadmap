fa9c9daca9b0d5d315dd96447a65d94806e1bbfd
esraamadi <esraa.sabry.mohamed@gmail.com>
2024-09-29 11:50:57 +0300

    Oct 2024

--- ulits/load_data.py
+++ ulits/load_data.py
@@ -48,7 +48,7 @@ def load_roadmap(file_name):
 
 def load_champian():
     # Open the spreadhseet
-    sheet = client.open_by_key('1WZFDx7IHvepqDLHFRcsfDaxBg-1UsSTpn40t1kVX1mg')
+    sheet = client.open_by_key('1H76vPO23u7t07VO8MVZUOs42OuflC0V-Vv1sXFjLkd0')
     
     # Initialize a dictionary to hold dataframes
     dataframes = {}

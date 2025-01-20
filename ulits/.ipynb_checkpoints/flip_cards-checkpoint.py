cc31e29be2e06eb16cf7ecbca8eebba06cb726b0
esraamadi <esraa.sabry.mohamed@gmail.com>
2025-01-20 14:31:37 +0300

    edit names

--- ulits/.ipynb_checkpoints/flip_cards-checkpoint.py
+++ ulits/.ipynb_checkpoints/flip_cards-checkpoint.py
@@ -78,7 +78,7 @@ def drow_cards():
             text-align: center;
         }
         .flip-card-front {
-            background-image: url("images/discord.png");
+            background-image: url("ulits/images/discord.png");
             color: white;
         }
         .flip-card-back {

f0735f98b0e478e2beff4e457c5b5453b285993e
esraamadi <esraa.sabry.mohamed@gmail.com>
2025-01-20 14:13:22 +0300

    edit names

--- ulits/flip_cards.py
+++ ulits/flip_cards.py
@@ -12,6 +12,7 @@ random.shuffle(students)
 # Flip the next card automatically every second
 def auto_flip():
     current_card = students[st.session_state.current_card]
+    print(st.session_state.current_card,students[st.session_state.current_card])
     st.session_state.flipped_cards[current_card] = not st.session_state.flipped_cards[current_card]
     st.session_state.current_card += 1
 

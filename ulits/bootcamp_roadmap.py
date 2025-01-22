193964b59aa9a81efb8fe22324e71a9cf8f6840a
esraamadi <esraa.sabry.mohamed@gmail.com>
2025-01-22 10:17:25 +0300

    edit names

--- ulits/bootcamp_roadmap.py
+++ ulits/bootcamp_roadmap.py
@@ -121,7 +121,7 @@ def _draw_groups(time, groups):
                 st.info(f'## Week {week_no} 🗓 - {phase}')
         st.subheader(f'Day {i[0][1]} : {_format_date(i[0][2])}')
         unit = day_agenda_df["Unit"]
-        st.caption(f" :blue[{unit}] {type(unit} :sunglasses:")
+        st.caption(f" :blue[{unit}] {type(unit)} :sunglasses:")
         item_count = 1
         for index, row in day_agenda_df.iterrows():
             st.write("")

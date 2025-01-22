0279e4e19fd6065297e02e7bf3a2bd3d45406864
esraamadi <esraa.sabry.mohamed@gmail.com>
2025-01-22 09:18:23 +0300

    edit names

--- ulits/bootcamp_roadmap.py
+++ ulits/bootcamp_roadmap.py
@@ -187,5 +187,5 @@ def get_roadmap_page():
     st.image("ulits/images/map.png")
     
     roadmap_df = _get_raw_roadmap()
-    #st.dataframe(roadmap_df.groupby(['#Week', '#Day', 'Date', 'Phase', 'Unit']))
+    roadmap_df.groupby(['#Week', '#Day', 'Date', 'Phase', 'Unit']))
     _draw_agenda(roadmap_df)

a0df70d48975d6ae9f1a026ecb694b00084b3c08
esraamadi <esraa.sabry.mohamed@gmail.com>
2025-01-22 12:45:25 +0300

    edit names

--- ulits/.ipynb_checkpoints/penguin_cards-checkpoint.py
+++ ulits/.ipynb_checkpoints/penguin_cards-checkpoint.py
@@ -75,8 +75,8 @@ def get_cards():
     # Initialize session state for flip state of each card
     if "flipped_cards" not in st.session_state:
         st.session_state.flipped_cards = [i=="1" for i in list(df_cards['Status'])]  # 7 unique cards
-    st.write(list(df_cards['Status']))
-    st.write([i==1 for i in list(df_cards['Status'])])
+    st.write(list(df_cards['Name']))
+    st.write(list(df_cards['Week num']))
     st.markdown(flip_css, unsafe_allow_html=True)
 
     # Display up to 3 Cards per Row
